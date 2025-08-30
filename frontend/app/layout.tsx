61bc242b95d83861f8d6a601478cca41d990d37d
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-08-30 12:45:19 +0530

    added middleware

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,25 @@
+"use client";
 import './global.css';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
+import { useEffect } from "react";
 
-export const metadata = {
-  title: 'Travel Agency',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Disable right click
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+    // Disable common copy/inspect shortcuts
+    document.addEventListener("keydown", (e) => {
+      if (
+        (e.ctrlKey && ["c", "u", "s", "x"].includes(e.key.toLowerCase()))  // Ctrl+C, U, S, X
+        // (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") 
+      ) {
+        e.preventDefault();
+      }
+    });
+  }, []);
   return (
     <html lang="en">
       <body>
