e85aabdea89876c4a01ee8f74b7cd5f7df0d5f7c
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-09-01 11:32:15 +0530

    Updated

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,24 +2,11 @@
 import './global.css';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
-import { useEffect } from "react";
+
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // Disable right click
-    document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-    // Disable common copy/inspect shortcuts
-    document.addEventListener("keydown", (e) => {
-      if (
-        (e.ctrlKey && ["c", "u", "s", "x"].includes(e.key.toLowerCase()))  // Ctrl+C, U, S, X
-        // (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") 
-      ) {
-        e.preventDefault();
-      }
-    });
-  }, []);
+ 
   return (
     <html lang="en">
       <body>
