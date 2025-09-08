0f485dd7b2f753df4af3ec75db1645aaf5f97459
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-09-08 16:41:02 +0530

    change galleryslider

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '../components/pages/Home/Hero';
 import WhyBest from '../components/pages/Home/WhyBest';
 import GallerySlider from '../components/pages/Home/GallerySlider';
+import { GalleryImage } from "@/components/types/galleryslider";
 import Contact from '@/components/pages/Contact/contact';
 import CommunityTrips from '../components/pages/Home/Upcomingtrip';
 import FeaturedPackages from '../components/pages/Home/FeaturedPackages';
@@ -11,7 +12,7 @@ import Stats from '../components/pages/Home/Stats';
 
 
 export default function HomePage() {
-  const images = [
+  const images: GalleryImage[] = [
     { url: "/images/leh.jpg", title: "leh", location: "leh" },
     { url: "/images/shimla.jpg", title: "shimla", location: "shimla" },
     { url: "/images/spain.jpg", title: "spain", location: "spain" },
@@ -25,18 +26,19 @@ export default function HomePage() {
     { url: "/images/spain.jpg", title: "spain", location: "spain" },
     { url: "/images/europe.jpg", title: "europe", location: "europe" },
   ];
+
   return (
     <>
       <Hero />
       <SliderBanner />
-      <International/> 
-      <CommunityTrips/>
-      <Romantic/>
+      <International /> 
+      <CommunityTrips />
+      <Romantic />
       <SliderBanner />
-      <FeaturedPackages/>
+      <FeaturedPackages />
       <WhyBest />
       <GallerySlider images={images} />
-      <Stats/>
+      <Stats />
       <Contact />
     </>
   );
